54f6d2e8802bf73f25095617d0cdb08039263254
Martin <martincapocasale@gmail.com>
2017-06-11 23:44:56 -0300

    prueba crear evento

---
+++ tp-calendario/spec/evento_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require_relative './model/evento'
+
+
+describe 'Evento' do 
+
+	it  'es posible crear un evento ' do
+		expect{Evento.new calendario, "1", "Reunion","2017-04-20T15:00:00-03:00","2017-04-20T16:00:00-03:00"}
+
+
+	end	
+
+
+
+
+end	
