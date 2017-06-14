cb0833f58756f76d204d3131bb20e6eb66faa8e4
Martin <martincapocasale@gmail.com>
2017-06-13 23:40:51 -0300

    se agrega test en validacionDenombre

--- tp-calendario/spec/validacion_de_nombre_calendario_spec.rb
+++ tp-calendario/spec/validacion_de_nombre_calendario_spec.rb
@@ -8,4 +8,10 @@ describe 'Validacion_de_nombre_calendario' do
 	it 'no es posible crear un calendario sin nombre' do
 	  expect{Calendario.new nil }.to raise_error(ExceptionValidacionDeNombreCalendario)
 	end
+
+	it 'Es posible crear un calendario con nombre CalendarioUniversitario' do
+	  valor_esperado = 'CalendarioUniversitario'
+	  calendar = Calendario.new (valor_esperado)
+	  expect(calendar.nombre).to eq valor_esperado
+	end
 end	
