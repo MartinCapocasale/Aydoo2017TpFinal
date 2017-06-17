aa2194531b4457ac333ea3191202bbaa6237fab9
Martin <martincapocasale@gmail.com>
2017-06-17 20:42:02 -0300

    refactor recurrencia

--- tp-calendario/model/gestor_de_recurrencia.rb
+++ tp-calendario/model/gestor_de_recurrencia.rb
@@ -1,18 +1,21 @@
 require_relative '../model/evento'
 require 'date'
 
-class GestorDeRecurrencia < Evento
+class GestorDeRecurrencia 
 
 	attr_accessor :frecuencia
-	attr_accessor :fin_de_fecha	
+	attr_accessor :fechaFin	
 	
-
-
-	def initialize (una_frecuencia,un_fin_de_fecha)
+	def initialize (una_frecuencia,una_fechaFin)
 			@frecuencia = una_frecuencia
-			@fin_de_fecha = Date.parse(un_fin_de_fecha)
+			@unafechaFin = DateTime.parse(una_fechaFin)
 	end
 
+	def obtener_Frecuencia
+	  	return @frecuencia
+	end
 
-
+    def obtener_Fecha_fin
+	    return @unafechaFin
+	end
 end
